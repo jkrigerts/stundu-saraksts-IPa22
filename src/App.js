24597140b3a7c301437a3f8bd717213b27f8d99d
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-09-25 14:48:43 +0300

    Pirmā stundu saraksta versija

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Diena from "./Diena";
+
+function App() {
+  return (
+    <>
+      <h1>Hi mom</h1>
+      <Diena nosaukums="pirmdiena" />
+      <Diena nosaukums="otrdiena" />
+      <Diena nosaukums="trešdiena" />
+    </>
+  );
+}
+
+export default App;
