e0cd21e610ab6f4b588883645cabae51c6246c8a
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-10 14:56:57 +0300

    Map at key

--- src/App.js
+++ src/App.js
@@ -4,38 +4,56 @@ function App() {
     "Datortīkli",
     "Datortīkli",
     "Sistēmu prog",
-    "Sistēmu prog"
+    "Sistēmu prog",
   ];
   const ceturdienasStundas = [
     "Sistēmu prog",
     "Matemātika",
     "Latviešu valoda",
-    "Vēl viena latviešu valoda + kultūra wtf lol"
+    "Vēl viena latviešu valoda + kultūra wtf lol",
   ];
   //kaut kādā veidā varbūt mapīti izmantot
   // kaiut kādu const map
-  // Kas ir katrai dienai? Stundas, Dienas Nosaukums
+  // Pieliec, lūdzu, vēl vienu dienu:
   const visuDienuStunduSaraksts = [
-   {
-    diena: "Trešdiena",
-    stundas: [
-      "Datortīkli",
-    "Datortīkli",
-    "Sistēmu prog",
-    "Sistēmu prog"
-    ]
-   }
-  ]
+    {
+      diena: "Trešdiena",
+      stundas: ["Datortīkli", "Datortīkli", "Sistēmu prog", "Sistēmu prog"],
+    },
+    {
+      diena: "Ceturtdiena",
+      stundas: [
+        "Programmēšana again",
+        "Matemātika",
+        "Latviešu valoda",
+        "Kultūra",
+      ],
+    },
+    {
+      diena: "Piektdiena",
+      stundas: ["Programmēšana", "Gribu mājās", "Eju mājās"],
+    },
+  ];
+
+  // Izmantojot piemēru no Diena.js, izmanto
+  // visuDienuStunduSaraksts un .map()!
+  const visaSarakstaJSX = visuDienuStunduSaraksts.map((diena) => {
+    return (
+      <Diena
+        key={diena.diena}
+        nosaukums={diena.diena}
+        stundas={diena.stundas}
+      />
+    );
+  });
   return (
     <>
       <h1>Hi mom</h1>
-      <Diena nosaukums="trešdiena" stundas={tresdienasStundas}/>
-      <Diena nosaukums="ceturtdiena" stundas={ceturdienasStundas}/>
+      {visaSarakstaJSX}
     </>
   );
 }
 
 export default App;
-
 
 // Pieliec trešdienai visas pārstundas
--- src/Diena.js
+++ src/Diena.js
@@ -1,36 +1,28 @@
 import Stunda from "./Stunda";
 
 function Diena(props) {
-  
-  const stunduSarakstsJSX = props.stundas.map((stunda) => {
-    return <Stunda name={stunda}/>
-  })
+  const stunduSarakstsJSX = props.stundas.map((stunda, indekss) => {
+    return <Stunda key={indekss} name={stunda} />;
+  });
 
   return (
     <>
       <p>Šodien ir {props.nosaukums}</p>
-      <ol>
-        {stunduSarakstsJSX}
-      </ol>
+      <ol>{stunduSarakstsJSX}</ol>
     </>
-  )
+  );
 }
 
 export default Diena;
-
-
 
 // Uztaisīt stundai komponenti
 
 // Lai ar props
 // tiek padots stundas nosaukums,
 
-
 // Vajadzīgas arī pirmdienas, otrdienas
 //stundas, bet jaunu komponeti netaisīsim.
 // Ko darīt?
-
-
 
 // .map() - iet cauri visiem masiva
 // elementiem, bet arī atgriež
