f270bd2a8b44897d30df489cf9eaba54ee7147ed
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-11-10 09:51:28 +0200

    Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
+
 import Diena from "./Diena";
 import style from "./App.module.css";
 function App() {
+  const [lessons, setLessons] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const tresdienasStundas = [
     "Datortīkli",
     "Datortīkli",
@@ -36,9 +41,41 @@ function App() {
     },
   ];
 
+  useEffect(() => {
+    async function getData() {
+      // Sūtīt pieprasījumu
+      const response = await fetch(
+        "https://cheese-cake.onthewifi.com/api/lessons"
+      );
+      const data = await response.json();
+      // Konsolē vēlos redzēt tikai
+      // IPa22 stundas un visu
+      console.log(data.IPa22);
+
+      const preparedData = [
+        { diena: "Pirmdiena", stundas: data.IPa22[0].classes },
+        { diena: "Otrdiena", stundas: data.IPa22[1].classes },
+        { diena: "Trešdiena", stundas: data.IPa22[2].classes },
+        { diena: "Ceturtdiena", stundas: data.IPa22[3].classes },
+        { diena: "Piektiena", stundas: data.IPa22[4].classes },
+        { diena: "Pirmdiena", stundas: data.IPa22[5].classes },
+        { diena: "Otrdiena", stundas: data.IPa22[6].classes },
+        { diena: "Trešdiena", stundas: data.IPa22[7].classes },
+        { diena: "Ceturtdiena", stundas: data.IPa22[8].classes },
+        { diena: "Piektiena", stundas: data.IPa22[9].classes },
+      ];
+      // Kas pietrūkst, lai JSX varētu attēlot datus?
+      setLessons(preparedData);
+      setLoading(false); // Uztaisīt JSX šo loading state...
+    }
+    getData();
+  }, []);
+
+  // Kur atrodas tava kursa stundas tajā API?
+
   // Izmantojot piemēru no Diena.js, izmanto
   // visuDienuStunduSaraksts un .map()!
-  const visaSarakstaJSX = visuDienuStunduSaraksts.map((diena) => {
+  const visaSarakstaJSX = lessons.map((diena) => {
     return (
       <Diena
         key={diena.diena}
@@ -50,7 +87,7 @@ function App() {
   return (
     <>
       <h1 className={style.hello}>Hi mom</h1>
-      {visaSarakstaJSX}
+      {loading ? <p>Loading...</p> : visaSarakstaJSX}
     </>
   );
 }
